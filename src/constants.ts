c8eec98cb097fca0d7a4fa2e44c7bd9fa04435cf
Łukasz Wójcik <lwojcik@lukem.net>
2021-11-17 10:32:00 +0100

    fix: support single quote in stylesheet link hrefs

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const PLUGIN_NAME = 'Styles';
 
 /** Match stylesheet link and get URL of file from HTML. */
-export const STYLESHEET_LINK_REGEXP = /<link\s+[^>]*href="([^"]+\.(?:css|scss|sass))"[^>]*>/g;
+export const STYLESHEET_LINK_REGEXP = /<link\s+[^>]*href=(?:'|")([^"]+\.(?:css|scss|sass))(?:'|")[^>]*>/g;
 
 export const DEFAULT_STYLES_DIRECTORY = 'styles';
 export const DEFAULT_SOURCE_DIRECTORY = 'src';
